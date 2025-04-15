2f2e109ac9c9e15016e1bf815b9dc6f1203dfa3e
mc-Corkell <katiemccorkell@gmail.com>
2025-04-15 15:54:44 -0700

    Change server port to 5000, install cors, modify client text displayed

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    fetch('http://localhost:5000/api/data')
+      .then(res => res.json())
+      .then(data => console.log(data));
+  }, []);
+
+  return <div>Katie's Basic Website Woohoo</div>;
 }
 
 export default App;
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const cors = require('cors');
+const port = 5000;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello, this is Katie!');
